14633aaa16a34f94fe4a2075a0f723e6957b44c7
bywayboy <bywayboy@qq.com>
2014-10-07 21:12:48 +0800

    some fixed

--- luci-app-phddns/luasrc/controller/phddns.lua
+++ luci-app-phddns/luasrc/controller/phddns.lua
@@ -31,11 +31,22 @@ function action_phddns_status()
 	local _ = luci.i18n.translate
 	local stat="Not Running"
 	local domain = ""
+	
+	function string_split(s, delimiter)
+		result = {};
+		for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+			table.insert(result, match);
+		end
+		return result;
+	end
+	local m
 	luci.http.prepare_content("application/json")
 	if nixio.fs.access(file) then
 		local str = nixio.fs.readfile(file)
-		stat, domain = str:match("([^|]+)|([^|]+)")
+		m = string_split(str,"|")
+		if #m > 0 then stat= m[1] end
+		if #m > 1 then domain= m[2] end
 	end
-	local arr = { ["stat"]=stat,["domain"]=domain }
+	local arr = { ["stat"]=_(stat), ["domain"]=domain }
 	luci.http.write_json(arr);	
 end
